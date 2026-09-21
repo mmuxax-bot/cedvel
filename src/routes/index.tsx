88afa4f01cdbb16eddf9f98aec17aaeba125c60e
Dev <dev@app.com>
2026-09-21 06:34:21 +0300

    Web əvəzinə əsl Native Flutter (Cedvel-Android) yükləndi

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { CedvelApp } from "@/components/cedvel/app";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <CedvelApp />;
-}
